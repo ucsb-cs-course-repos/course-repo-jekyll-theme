9a828f98e04bf423c3b45a5faa543acc20dbb02f
Phill Conrad <pconrad@cs.ucsb.edu>
2019-01-03 08:19:23 -0800

    add select-section  js to dev channel

--- assets/js/site.js
+++ assets/js/site.js
@@ -66,3 +66,40 @@ $(document).ready(function(){
 
 	       
 });
+
+
+function toggle_nav_items(o) {
+    /*  o has form { "text": "W19 Mirza", "url": "..."} */
+    $("#offerings-dropdown").text(o.text);
+    $(".nav-item").each( function() {
+	var term=$(this).data("term");
+	if (term) {
+	    if (term==o.text) {
+		$(this).removeClass("d-none");
+	    } else {
+		$(this).addClass("d-none");
+	    }
+	}
+    });
+}
+
+$( document ).ready(function() {
+    var offering = localStorage.getItem("offering");
+    if (offering!==null) {
+	var o = JSON.parse(offering);
+	toggle_nav_items(o);
+    }
+    $(".offerings-dropdown-item").click(
+	function(){
+	    var o = {
+		"text" : $(this).text(),
+		"url" : $(this).data("url")
+	    };
+	    toggle_nav_items(o);
+	    var offering = JSON.stringify(o);
+	    console.log("offering="+offering);
+	    localStorage.setItem("offering",offering);
+	    window.location = o.url;
+	}
+    );
+});
