0ee6d446ca27c146a34114e5feb77a657d0340d9
Phill Conrad <pconrad@cs.ucsb.edu>
2019-01-03 09:09:00 -0800

    still working on nav-json

--- _includes/nav-json.js
+++ _includes/nav-json.js
@@ -16,20 +16,27 @@ var nav = {
     },
     {% endif %}
     {% if site.data.navigation.offering_links %}
-    "offering_links": {
-	"title" : "{{ site.data.navigation.offerings.title }}",
-	"url" : "{{ site.data.navigation.offerings.url }}",
-	"baseurl" : "{{ site.data.navigation.offerings.baseurl }}",
-	"items" : [
-	    {% for item in site.data.navigation.offering_links.items %}
-	    {
-		"title" : "{{ item.title }}",
-		"url" : "{{ item.url }}",
-		"baseurl" : "{{ item.baseurl }}",
-	    },
-	    {% endfor %}
-	],
-    },
+    "offering_links": [
+	{% for item in site.data.navigation.offering_links %}
+		{
+		    "title": "{{item.title}}",		    
+		    {% if item.url %} "url": "{{item.url}}", {% endif %}		    
+		    {% if item.description %} "description": "{{item.description}}", {% endif %}
+		    {% if item.dropdown %}
+		    "dropdown" : [
+			{% for ditem in item.dropdown %}
+			{
+			    "title": "{{ditem.title}}",		    
+			    "url": "{{ditem.url}}", 
+			    {% if ditem.description %} "description": "{{ditem.description}}", {% endif %}
+			},
+			{% endfor %}
+		    ]
+		    {% endif %}		    
+		},
+	{% endfor %}
+     
+    ],
     {% endif %}
     {% if site.data.navigation.offering_menus %}
     "offering_menus" : [
