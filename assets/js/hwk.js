a88a3c5e6303607d92e721f15a0b487ee0de5095
Phill Conrad <pconrad@cs.ucsb.edu>
2019-10-27 17:46:05 -0700

    pc - fix homework headers

--- assets/js/hwk.js
+++ assets/js/hwk.js
@@ -7,12 +7,18 @@ layout: null
 
 $(document).ready(function(){
 
+    
     $('.pagebreak').each(function() {
 	$('.hwk-page-header-template').first().clone().appendTo($(this));
     });
 
     $('td.page-num').each(function(i) {
 	$(this).html(i+1); // re-calculate page numbers
+    });
+
+    $('.d-none').each(function(e) {
+	console.log("removing element with class .d-none");
+	e.remove();
     });
     
 });
